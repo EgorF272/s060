69ecf8964bb3dc5110ced1d4d8725bf6f5467b65
EgorF272 <144844556+EgorF272@users.noreply.github.com>
2023-12-05 23:39:30 +0300

    Update j5.js

--- j5.js
+++ j5.js
@@ -1,28 +1,63 @@
-function calc() {
-   //ссылка на элемент Select (Тип)
-   let type_tovara = document.getElementById("tovar");
-   //ссылка на элемент input (Кол-во)
-   let count = document.getElementById("count");
-   //ссылка на элемент span, в него пишем стоимость
-   let result = document.getElementById("result");
-   let price = 0;
-   price += parseInt(type_tovara.options[type_tovara.selectedIndex].value);
-   if(count.value>0)
-   price = parseInt(count.value) * price;
-   else alert("Укажите верное количество");
-   result.innerHTML = price;
-}
-function sbros()
-{
-   let result = document.getElementById("result");
-   let price=0;
-   result.innerHTML=price;
-}
-window.addEventListener('DOMContentLoaded', function () {
-   console.log("DOM fully loaded and parsed");
-   let b = document.getElementById("button");
-   let c = document.getElementById("button2");
-   
-   b.addEventListener("click", calc);
-   c.addEventListener("click", sbros);
-});
+window.addEventListener("DOMContentLoaded", function(){
+   const quantityInput = document.getElementById("quantity");
+   const serviceTypeInputs = document.querySelectorAll('input[name="serviceType"]');
+   const optionsSection = document.getElementById("optionsSection");
+   const optionsSelect = document.getElementById("options");
+   const propertySection = document.getElementById("propertySection");
+   const propertyCheckbox = document.getElementById("property");
+   const totalPriceSpan = document.getElementById("totalPrice");
+ 
+   const pricePerUnit = {
+       type1: 5000, 
+       type2: 8000,
+       type3: 13000
+   };
+ 
+   const optionPrices = {
+       option1: 20000, 
+       option2: 10000,
+       option3: 0
+   };
+ 
+   function calculateTotalPrice() {
+      const quantity = parseFloat(quantityInput.value);
+  
+      // Проверка на положительное значение
+      if (isNaN(quantity) || quantity <= 0) {
+          totalPriceSpan.textContent = "Введите корректное количество.";
+          return;
+      }
+  
+      const selectedServiceType = Array.from(serviceTypeInputs).find((input) => input.checked).value;
+  
+      let total = pricePerUnit[selectedServiceType] * quantity;
+  
+      if (selectedServiceType === 'type1') {
+          propertySection.style.display = "none";
+          optionsSection.style.display = "none";
+      }
+  
+      if (selectedServiceType === 'type2') {
+          propertySection.style.display = "none";
+          optionsSection.style.display = "block";
+          const selectedOption = optionsSelect.value;
+          total += optionPrices[selectedOption];
+      }
+  
+      if (selectedServiceType === 'type3') {
+          optionsSection.style.display = "none";
+          propertySection.style.display = "block";
+          if (propertyCheckbox.checked) {
+              total += 1200 * quantity;
+          }
+      }
+  
+      totalPriceSpan.textContent = total;
+  }
+  
+ 
+   quantityInput.addEventListener("input", calculateTotalPrice);
+   serviceTypeInputs.forEach(input => input.addEventListener("change", calculateTotalPrice));
+   optionsSelect.addEventListener("change", calculateTotalPrice);
+   propertyCheckbox.addEventListener("change", calculateTotalPrice);
+ });
